94b7814b7a3117b3e54d47bd26a3675653dfcbfc
JoseManuel145 <233328@ids.upchiapas.edu.mx>
2025-11-22 22:13:41 -0600

    Add: Implementacion de la entidad User

--- src/core/logging.py
+++ src/core/logging.py
@@ -2,11 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(name)s - %(message)s",
-    handlers=[
-        logging.FileHandler("app.log"),
-        logging.StreamHandler()
-    ]
+    format="%(asctime)s %(levelname)s %(name)s %(message)s"
 )
 
 logger = logging.getLogger("app")
--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-
+from src.api.routers.user_routes import router as user_router
 app = FastAPI(
     title="Golazo",
     description="Backend del e-commerce Golazo",
-    version="1.0.0"
+    version="0.0.2"
 )
-
+app.include_router(user_router)
 @app.get("/")
 def root():
     return {"message": "Bienvenido a Golazo"}
