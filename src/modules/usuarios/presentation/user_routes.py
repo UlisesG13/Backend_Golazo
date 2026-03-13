ceaca2a29f18987a8f888ca4a516a1f271be67fa
JoseManuel145 <233328@ids.upchiapas.edu.mx>
2026-03-13 11:46:43 -0600

    Fix: add delete admin route

--- src/modules/usuarios/presentation/user_routes.py
+++ src/modules/usuarios/presentation/user_routes.py
@@ -63,6 +63,13 @@ def create_admin(
 ):
     return uc.execute(user)
 
+@router.delete("/admin/{usuario_id}", response_model=LoginResponseDTO)
+def delete_admin(
+        usuario_id: str,
+        uc: DeleteUser = Depends(delete_user_service),
+):
+    return uc.execute(usuario_id)
+
 @router.get("/admins", response_model=List[UserDTO])
 def get_admins(
         uc: GetAllAdmins = Depends(get_all_admins_service)
